4bf465f4dd3d0d4a7e15c16a25b130177f10b0f4
Arthur Neves <82537546+ArthurNeves0512@users.noreply.github.com>
2026-06-02 05:58:50 -0300

    aprendendo GTK ainda

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import gi
-
+import sys
 gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk
+gi.require_version("Adw",'1')
+from gi.repository import  Gtk,Adw
 
 
-class MyApplication(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.MyGtkApplication")
-        GLib.set_application_name('My Gtk Application')
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self,*agrs,**kwargs):
+        super().__init__(*agrs,**kwargs)
+       
+        self.set_default_size(800,400)
+        self.set_title("Simulador de Comunicação Digital")
 
-    def do_activate(self):
-        window = Gtk.ApplicationWindow(application=self, title="Hello World")
-        window.present()
+        self.configurationHeaderBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(self.configurationHeaderBox)
+
+        self.initButton = Gtk.Button(label="Iniciar Simulação")
+        self.configurationHeaderBox.append(self.initButton)
+        self.initButton.connect("clicked",self.print)
+
+        self.resetButton = Gtk.Button(label="Resetar")
+        self.configurationHeaderBox.append(self.resetButton)
+        self.resetButton.connect("clicked",self.print)
+        
+
+    
+    
+    def print(self,button):
+        print("UHUUUUU")
+
+class MyApp(Adw.Application):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate',self.on_active)
 
 
-app = MyApplication()
-app.run()
+    def on_active(self,app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+
+
+app = MyApp(application_id="com.example.GtkApplication")
+app.run(sys.argv)
